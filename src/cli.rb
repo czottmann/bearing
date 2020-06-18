88d922484ffaf4c9e16f4bdda26f8139994a24a3
Carlo Zottmann <carlo@municode.de>
2020-06-18 11:21:25 +0200

    [NEW] Moves wait-for-incoming-data code into Bearing::Comms namespace

--- src/cli.rb
+++ src/cli.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/ruby
 
 require 'securerandom'
-require 'timeout'
 
 require_relative 'config.rb'
 require_relative 'lib/bearing-cmd.rb'
@@ -15,30 +14,7 @@ Bearing::Cmd.start_app
 Bearing::Cmd.call_bear(
   action: action,
   call_id: call_id,
-  params: Bearing::Cmd.translate_args_to_hash(args)
+  params: Bearing::Cmd.translate_args_to_hash(args),
 )
 
-json_data = nil
-
-begin
-  res = nil
-  tmp_filename = Bearing::Comms.unique_tmp_file(call_id)
-
-  Timeout::timeout(3) do
-    while !res do
-      if File.exist?(tmp_filename)
-        res = File.read(tmp_filename)
-      else
-        sleep 0.4
-      end
-    end
-  end
-
-  json_data = res
-rescue Timeout::Error
-  json_data = ({ _success: false, _timeout: true }).to_json
-end
-
-Bearing::Comms.remove_tmp_folder(call_id)
-
-puts json_data
+puts Bearing::Comms.wait_for_incoming_data(call_id)
--- src/lib/bearing-comms.rb
+++ src/lib/bearing-comms.rb
@@ -3,6 +3,7 @@
 require 'cgi'
 require 'fileutils'
 require 'json'
+require 'timeout'
 require 'uri'
 
 require_relative '../config.rb'
@@ -10,16 +11,6 @@ require_relative '../config.rb'
 module Bearing
   module Comms
     class << self
-      def write_incoming_data_to_tmp_file(uri_string = '')
-        uri = URI.parse(uri_string)
-        call_id = uri.host
-        is_success = (uri.path == '/success')
-        tmp_filename = unique_tmp_file(call_id)
-        output = query_to_json(uri.query, success: is_success)
-
-        File.open(tmp_filename, 'w') { |f| f.puts output }
-      end
-
       def query_to_json(query_str = '', success: false)
         res = Hash[
           CGI::parse(query_str).map do |k, v|
@@ -43,6 +34,40 @@ module Bearing
         tmp_path = "/tmp/#{::APP_BUNDLE_ID}/#{call_id}"
         FileUtils.mkdir_p(tmp_path, mode: 0700) unless Dir.exist?(tmp_path)
         tmp_path
+      end
+
+      def wait_for_incoming_data(call_id = '')
+        results = begin
+                    res = nil
+                    tmp_filename = unique_tmp_file(call_id)
+
+                    Timeout::timeout(3) do
+                      while !res do
+                        if File.exist?(tmp_filename)
+                          res = File.read(tmp_filename)
+                        else
+                          sleep 0.4
+                        end
+                      end
+                    end
+
+                    res
+                  rescue Timeout::Error
+                    ({ _success: false, _timeout: true }).to_json
+                  end
+
+        remove_tmp_folder(call_id)
+        results
+      end
+
+      def write_incoming_data_to_tmp_file(uri_string = '')
+        uri = URI.parse(uri_string)
+        call_id = uri.host
+        is_success = (uri.path == '/success')
+        tmp_filename = unique_tmp_file(call_id)
+        output = query_to_json(uri.query, success: is_success)
+
+        File.open(tmp_filename, 'w') { |f| f.puts output }
       end
     end
   end
