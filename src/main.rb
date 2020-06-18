376e345bf3a828e853f89415371aca7c354a3c07
Carlo Zottmann <carlo@municode.de>
2020-06-18 11:23:55 +0200

    [CHG] Changes formatting

--- src/main.rb
+++ src/main.rb
@@ -9,13 +9,10 @@ incoming = ARGV[0]
 
 if !incoming
   Bearing::Menu.print_items
-
 elsif incoming.start_with?(::URI_SCHEME)
   Bearing::Comms.write_incoming_data_to_tmp_file(incoming)
-
 elsif incoming.start_with?('Install')
   Bearing::ShellIntegration.install
-
 elsif incoming.start_with?('Uninstall')
   Bearing::ShellIntegration.uninstall
 end
