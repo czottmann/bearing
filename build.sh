f3acc4a40d3ac4dc1d277837dad4ecc68fa0032e
Carlo Zottmann <carlo@municode.de>
2020-06-19 12:20:05 +0200

    [NEW] Uses app configuration variables for building

--- build.sh
+++ build.sh
@@ -3,20 +3,25 @@
 rm -rf ./dist
 mkdir ./dist
 
+config_var() {
+  ruby -e "require './src/config.rb'; puts $1"
+}
+
 /usr/local/bin/platypus \
-  --app-icon '/Applications/Platypus.app/Contents/Resources/PlatypusDefault.icns'  \
+  --app-icon '/Applications/Platypus.app/Contents/Resources/PlatypusDefault.icns' \
+  --app-version "$(config_var VERSION)" \
   --author 'Carlo Zottmann <carlo@zottmann.org>' \
-  --bundle-identifier 'org.zottmann.Bearing' \
   --background \
+  --bundle-identifier "$(config_var APP_BUNDLE_ID)" \
   --bundled-file ./src \
   --interface-type 'Status Menu'  \
   --interpreter '/usr/bin/ruby'  \
   --name 'Bearing'  \
   --overwrite \
   --status-item-kind 'Text' \
-  --status-item-title 'B' \
   --status-item-sysfont \
-  --uri-schemes 'bearing' \
+  --status-item-title 'B' \
+  --uri-schemes "$(config_var URI_SCHEME)" \
   ./src/main.rb
 
 mv ./src/Bearing.app ./dist/
